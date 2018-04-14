cc5d577c3cafc6297e7d492ab99df435f2d708eb
Karina Lund <toyotac7@gmail.com>
2018-04-14 20:25:29 +0200

    Extends game functionality
    Added lifes and points.

--- js/app.js
+++ js/app.js
@@ -5,6 +5,14 @@ const
   sectionX = 101,
   playerInitPos = [2, 5];
 
+// var GameElements = function() {
+//
+// }
+//
+// Enemy.prototype.render = function() {
+//   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+// };
+
 // Enemies our player must avoid
 var Enemy = function() {
   // Variables applied to each of our instances go here,
@@ -13,14 +21,30 @@ var Enemy = function() {
   // The image/sprite for our enemies, this uses
   // a helper we've provided to easily load images
   this.sprite = 'images/enemy-bug.png';
-  this.x = 0;
-  this.y = 100;
+  this.startPosX = sectionY * -1;
+  this.x = this.startPosX;
+  this.randomizePosition();
+  this.randomizeSpeed();
 };
+
+Object.defineProperty(Enemy.prototype, 'y', {
+  get: function() {
+    return (this.row - 0.5) * sectionY;
+  }
+});
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-  this.x += 5 * dt;
+  this.x += this.speed * dt;
+  if (this.checkCollision()) {
+    game.lifeLoss();
+    player.resetPlayer();
+  }
+  if (this.isEndOfField()) {
+    this.moveToStart();
+  }
+
   // You should multiply any movement by the dt parameter
   // which will ensure the game runs at the same speed for
   // all computers.
@@ -31,6 +55,30 @@ Enemy.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+Enemy.prototype.randomizeSpeed = function() {
+  // Enemy speed is a random number from 100 to 500
+  this.speed = Math.floor(Math.random() * 450) + 100;
+};
+
+Enemy.prototype.randomizePosition = function() {
+  this.row = Math.floor(Math.random() * 3) + 1;
+};
+
+Enemy.prototype.checkCollision = function() {
+  return (this.row === player.row) && (player.x - 25 < this.x + sectionX / 2) &&
+    (player.x + 5 > this.x - sectionX / 2);
+};
+
+Enemy.prototype.isEndOfField = function() {
+  return this.x > sectionY * (numCols + 1);
+};
+
+Enemy.prototype.moveToStart = function() {
+  this.randomizeSpeed();
+  this.randomizePosition();
+  this.x = this.startPosX;
+};
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
@@ -39,18 +87,18 @@ var Player = function() {
   this.resetPlayer();
 };
 
-Object.defineProperty(Player.prototype, "x", {
-  get: function() {
-    return this.col * sectionX;
+Object.defineProperties(Player.prototype, {
+  'x': {
+    get: function() {
+      return this.col * sectionX;
+    }
+  },
+  'y': {
+    get: function() {
+      return (this.row - 0.5) * sectionY;
+    }
   }
 });
-
-Object.defineProperty(Player.prototype, "y", {
-  get: function() {
-    return (this.row - 0.5) * sectionY;
-  }
-});
-
 
 Player.prototype.update = function(dt) {
 
@@ -94,8 +142,66 @@ Player.prototype.handleInput = function(direction) {
   this.movePlayer(direction);
   if (this.row === 0) {
     this.resetPlayer();
+    game.addPoints(100);
   }
 };
+
+var Game = function() {
+  this.lifesMax = 6;
+  this.lifeSprite = 'images/Life.png';
+  this.lifeLostSprite = 'images/LifeLost.png';
+  this.resetGame();
+};
+
+Game.prototype.resetGame = function() {
+  this.lifes = this.lifesMax;
+  this.points = 0;
+};
+
+Game.prototype.lifeLoss = function() {
+  if (this.lifes > 0) {
+    this.lifes--;
+  }
+};
+
+Game.prototype.addPoints = function(pointValue) {
+  this.points += pointValue;
+  if (this.points > 9999999) {
+    this.points = 9999999;
+  }
+};
+
+Game.prototype.render = function() {
+  ctx.font = "25px Alien Encounters";
+  ctx.fillText("Life: ", 5, 30);
+  for (var heart = 0; heart < this.lifesMax; heart++) {
+    if (this.lifesMax - this.lifes > heart) {
+      ctx.drawImage(Resources.get(this.lifeLostSprite), heart * 30 + 70, 5);
+    } else {
+      ctx.drawImage(Resources.get(this.lifeSprite), heart * 30 + 70, 5);
+    }
+  }
+  ctx.fillText("Points: " + String(this.points).padStart(7, "0"), 300, 30);
+}
+
+var Rock = function() {
+  this.sprite = 'images/Rock.png';
+  this.generate();
+};
+
+Rock.prototype.generate = function() {
+  this.visible = Math.random() >= 0.5;
+  this.col = Math.floor(Math.random() * 4) + 0;
+  this.row = Math.floor(Math.random() * 3) + 1;
+};
+
+Rock.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+function render() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -105,6 +211,8 @@ var enemyMatilda = new Enemy();
 var enemyFrank = new Enemy();
 var allEnemies = [enemyBob, enemyMatilda, enemyFrank];
 var player = new Player();
+var game = new Game();
+var rock = new Rock();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
